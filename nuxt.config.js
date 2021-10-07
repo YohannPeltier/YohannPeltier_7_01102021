3e5898c14b6898c13f71f7867ba9bc7b216e067e
Yohann Peltier <yohannpeltier@gmail.com>
2021-10-07 17:09:40 +0200

    build: add axios baseURL

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,9 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    baseURL: process.env.BASE_URL || 'http://localhost:3000',
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
