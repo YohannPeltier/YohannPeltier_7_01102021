6ccac8c608ddd85bc864f6d4a7912442dcd442be
Yohann Peltier <yohannpeltier@gmail.com>
2021-10-13 14:34:46 +0200

    fix(front): add focus on personalize input file

--- components/message/MessagePost.vue
+++ components/message/MessagePost.vue
@@ -4,40 +4,50 @@
     ref="form"
     enctype="multipart/form-data"
   >
-    <b-card
-      tag="article"
-      footer-tag="footer"
-      class="shadow-sm mx-auto mt-3 mb-4 w-9"
-    >
-      <b-card-text class="p-0">
-        <b-form-textarea
-          v-model="content"
-          placeholder="Exprimez-vous"
-          rows="3"
-          max-rows="8"
-          maxlength="255"
-          required
-        ></b-form-textarea>
-      </b-card-text>
-      <template #footer>
-        <div class="d-flex justify-content-between ml-n2 mr-n2">
-          <label for="file-upload" class="btn btn-light">
-            <b-icon icon="image" aria-hidden="true"></b-icon>
-            <span class="ml-2"> Ajouter une image </span>
-          </label>
-          <input
-            @change="onChangeImage"
-            id="file-upload"
-            ref="image"
-            type="file"
-            accept=".jpeg, .jpg, .png"
-          />
-          <b-button type="submit" variant="primary" class="pl-4 pr-4">
-            Publier
-          </b-button>
-        </div>
-      </template>
-    </b-card>
+    <b-overlay :show="loader" rounded="sm">
+      <b-card
+        tag="article"
+        footer-tag="footer"
+        class="shadow-sm mx-auto mt-3 mb-4 w-9"
+      >
+        <b-card-text class="p-0">
+          <b-form-textarea
+            v-model="content"
+            @input="onChangeContent"
+            placeholder="Exprimez-vous"
+            rows="3"
+            maxlength="255"
+            no-resize
+          ></b-form-textarea>
+        </b-card-text>
+        <template #footer>
+          <div class="d-flex justify-content-between ml-n2 mr-n2">
+            <input
+              @change="onChangeImage"
+              id="file-upload"
+              ref="inputImage"
+              type="file"
+              accept=".jpeg, .jpg, .png, .gif"
+            />
+            <label
+              for="file-upload"
+              class="d-flex align-items-center btn btn-light mr-3"
+            >
+              <b-icon icon="image" aria-hidden="true"></b-icon>
+              <span class="ml-2 text-break">{{ labelInputImage }}</span>
+            </label>
+            <b-button
+              ref="buttonPostMessage"
+              type="submit"
+              variant="primary"
+              class="pl-4 pr-4"
+            >
+              Publier
+            </b-button>
+          </div>
+        </template>
+      </b-card>
+    </b-overlay>
   </b-form>
 </template>
 
@@ -51,13 +61,25 @@
 </style>
 <style scoped>
 input[type='file'] {
-  display: none;
+  border: 0;
+  clip: rect(0);
+  height: 0;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  width: 0;
 }
 label {
   margin: 0;
 }
 textarea {
   border: none;
+}
+button:focus:not(.focus-visible) {
+  outline: 0;
+}
+input[type='file']:focus ~ label {
+  box-shadow: 0 0 0 0.2rem rgb(38 143 255 / 50%);
 }
 .card-body {
   padding: 0;
@@ -88,10 +110,14 @@ export default {
     return {
       content: '',
       image: null,
+      labelInputImage: 'Ajouter une image...',
+      loader: false,
     };
   },
   methods: {
     async postMessage() {
+      this.loader = true;
+      this.$refs.buttonPostMessage.disabled = true;
       const formData = new FormData();
       formData.append('content', this.content);
       formData.append('messageImage', this.image);
@@ -101,18 +127,41 @@ export default {
         .then((res) => {
           this.content = '';
           this.image = null;
+          this.$refs.inputImage.value = '';
+          this.labelInputImage = 'Ajouter une images...';
+          console.log(res.data);
           this.$parent.newMessage(res.data);
+          this.loader = false;
         })
         .catch((error) => {
-          if (error.response.data.error !== '') console.log(error);
+          if (error !== '') console.log(error);
         });
     },
     onChangeImage() {
-      const file = this.$refs.image.files[0];
+      const file = this.$refs.inputImage.files[0];
       if (file) {
+        this.labelInputImage = file.name;
         this.image = file;
+        this.$refs.buttonPostMessage.disabled = false;
+      } else {
+        if (!this.content) {
+          this.$refs.buttonPostMessage.disabled = true;
+        }
+        this.labelInputImage = 'Ajouter une images...';
       }
     },
+    onChangeContent() {
+      if (this.content) {
+        this.$refs.buttonPostMessage.disabled = false;
+      } else {
+        if (!this.image) {
+          this.$refs.buttonPostMessage.disabled = true;
+        }
+      }
+    },
+  },
+  mounted() {
+    this.$refs.buttonPostMessage.disabled = true;
   },
 };
 </script>
